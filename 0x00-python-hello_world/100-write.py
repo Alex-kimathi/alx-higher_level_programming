a5f5a9aaecb34ae2d5c75b4af3acb1edbe327634
Alex-kimathi <alex2kimathi@gmail.com>
2023-11-28 06:17:38 +0000

    prints using write

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+# Using the sys module to write to stderr
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# Exiting with the status code 1
+sys.exit(1)
