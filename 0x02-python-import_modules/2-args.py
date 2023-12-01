3068d71096a0deda97777780e3159922942edb7d
Alex-kimathi <alex2kimathi@gmail.com>
2023-12-01 14:13:38 +0000

    prints all number and arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
 
     number = len(sys.argv) - 1
     if number == 0:
-        print("0 arguements.")
+        print("0 arguments.")
     elif number == 1:
-        print("1 arguement:")
+        print("1 argument:")
     else:
-        print("{} arguemnts:".format(number))
+        print("{} arguments:".format(number))
     for i in range(number):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
