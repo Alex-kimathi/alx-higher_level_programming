4e325121cf39759abe1c2f64cdc55a2d75ede5eb
Alex-kimathi <alex2kimathi@gmail.com>
2023-12-01 08:06:48 +0000

    performs calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     """print the substration"""
     print("{} - {} = {}".format(a, b, sub(a, b)))
