90c0ba4e2ea015fc9e764e282912f7e79e5be05c
ALEX KIMATHI <136745775+Alex-kimathi@users.noreply.github.com>
2024-05-23 01:07:32 +0300

    Creates a table with two columns

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Creates table called first_table
 -- assigns two columns id INT, name(Varchar)
-CREATE DATABASE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
 );
