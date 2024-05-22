dbbd48fcf6e0d13c832c8602f89d283ea6f5a4a2
ALEX KIMATHI <136745775+Alex-kimathi@users.noreply.github.com>
2024-05-23 01:14:26 +0300

    prints table information

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,14 +1,3 @@
 -- prints the table information from database
 -- does not use explain or desctibe
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_NAME = 'first_table' 
-    AND TABLE_SCHEMA = DATABASE();
+SHOW CREATE TABLE first_table;
