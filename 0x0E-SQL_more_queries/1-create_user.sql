c19ac85d6b8813aaa4d2c1ce9a4334716168ad51
ALEX KIMATHI <136745775+Alex-kimathi@users.noreply.github.com>
2024-05-23 03:22:39 +0300

    creates a user user_0d_1 and grants all the previleges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a user user_0d_1 and grants all the previleges
+-- user password is user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
